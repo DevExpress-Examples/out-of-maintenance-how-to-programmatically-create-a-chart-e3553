65b51ce7714e7bf17316c0c93959d3eb12ad9a31
DevExpressExampleBot <fakeEmail>
2022-12-18 20:17:24 +0400

    Source auto update [skip ci]

--- VB/DXCharts_Runtime/MainWindow.xaml.vb
+++ VB/DXCharts_Runtime/MainWindow.xaml.vb
@@ -1,38 +1,32 @@
-﻿Imports System.Collections
 Imports System.Windows
 Imports DevExpress.Xpf.Charts
 
 Namespace DXCharts_Runtime
 
-	Partial Public Class MainWindow
-		Inherits Window
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			' Create a chart.
-			Dim chart As New ChartControl()
-
-			' Create a diagram.
-			Dim diagram As New XYDiagram2D()
-			chart.Diagram = diagram
-
-			' Create a bar series.
-			Dim series As New BarSideBySideSeries2D()
-			diagram.Series.Add(series)
-
-			' Add points to the series.
-			series.Points.Add(New SeriesPoint("A", 1))
-			series.Points.Add(New SeriesPoint("B", 3))
-			series.Points.Add(New SeriesPoint("C", 5))
-			series.Points.Add(New SeriesPoint("D", 2))
-			series.Points.Add(New SeriesPoint("E", 7))
-
-			' Add the chart to the window.
-			Me.Content = chart
-		End Sub
-
-	End Class
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            ' Create a chart.
+            Dim chart As ChartControl = New ChartControl()
+            ' Create a diagram.
+            Dim diagram As XYDiagram2D = New XYDiagram2D()
+            chart.Diagram = diagram
+            ' Create a bar series.
+            Dim series As BarSideBySideSeries2D = New BarSideBySideSeries2D()
+            diagram.Series.Add(series)
+            ' Add points to the series.
+            series.Points.Add(New SeriesPoint("A", 1))
+            series.Points.Add(New SeriesPoint("B", 3))
+            series.Points.Add(New SeriesPoint("C", 5))
+            series.Points.Add(New SeriesPoint("D", 2))
+            series.Points.Add(New SeriesPoint("E", 7))
+            ' Add the chart to the window.
+            Content = chart
+        End Sub
+    End Class
 End Namespace
